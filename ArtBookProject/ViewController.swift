3a919466cf3fb4b144639c7952f7ce8d24260f42
Fulya Sariyar <fulyasariyar@outlook.com>
2022-07-01 17:42:06 +0300

    Core Data

---
+++ ArtBookProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtBookProject
+//
+//  Created by Fulya Sarıyar on 1.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
